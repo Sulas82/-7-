b0a22395f1239b3e04ccb022a3bca7e0ddf9afd8
kislyyparen <limon.umidaev@gmail.com>
2025-10-21 09:34:17 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+static double Average(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+    return sum /= arr.Length;
+}
