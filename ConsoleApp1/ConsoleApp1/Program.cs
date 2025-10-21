71fe4a9d91c1f6870fe09dde7ef0471938e44446
Sulas828 <sultanovaskar2008@gmail.com>
2025-10-21 09:36:17 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+static int[] Arrays(int[] arr, string name)
+{
+    Random rnd = new Random();
+    Console.WriteLine($"Массив {name}:");
+    for (int i = 0; i <= arr.Length - 1; ++i)
+    {
+        arr[i] = rnd.Next(-100, 300);
+        Console.Write($" {arr[i]}");
+    }
+    Console.WriteLine();
+    int[] result = new int[arr.Length];
+    return result;
+}
